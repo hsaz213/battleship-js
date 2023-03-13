99e4ffb6c8d56275fc1174d386c0052d815ccaa8
Sandor Hajdu <125272342+hsaz213@users.noreply.github.com>
2023-03-13 12:42:50 +0100

    merging

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const board = [["","","",""],["","","","0"],["q","","",""],["","","",""]];
+const board = [["", "", "", ""], ["", "", "", "0"], ["q", "", "", ""], ["", "", "", ""]];
 
 function selectGame(data) {
   displayMessage(data, "black");
@@ -9,19 +9,19 @@ function handleClick(data) {
 }
 
 function resetGame() {
-  for(let i = 0; i < board.length; i++) {
-    for(let j = 0; j < board[i].length; j++) {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
       board[i][j] = "";
     }
   }
-  displayBoard({boardnumber: 1,board: board});
+  displayBoard({ boardnumber: 1, board: board });
 }
 
 function aiShoot(data) {
-  
+
 }
 
-displayBoard({boardnumber: 1,board: board});
-displayBoard({boardnumber: 2,board: board});
+displayBoard({ boardnumber: 1, board: board });
+displayBoard({ boardnumber: 2, board: board });
 displayMessage("message", "green");
 displayTextMessage("text message", "red");
